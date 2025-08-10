32a9d9965364f8e0a9d172a08a4f0db0fe6f6ead
Ali <107561030+A-li-gena@users.noreply.github.com>
2025-08-10 19:12:30 +0500

    Add files via upload

---
+++ @angular_material_button.js
@@ -0,0 +1,35 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-7KK7OCU5.js";
+import "./chunk-XPTVDSGO.js";
+import "./chunk-FQEM7BSC.js";
+import "./chunk-SF7Q7RME.js";
+import "./chunk-SDQMWN4J.js";
+import "./chunk-6UHCRHZ7.js";
+import "./chunk-DSK7TZNG.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
